a99e09c1a284573d9392ea6a296eea255a510382
wolfzex <yuriy.maliy.email@gmail.com>
2025-11-27 22:09:24 +0200

    Reset migrations and add initial schema

--- migrations/versions/71415cf8670f_initial_schema.py
+++ migrations/versions/71415cf8670f_initial_schema.py
@@ -1,8 +1,8 @@
-"""Initial migration
+"""Initial schema
 
-Revision ID: 77e77aae3811
+Revision ID: 71415cf8670f
 Revises: 
-Create Date: 2025-11-02 15:08:20.557710
+Create Date: 2025-11-27 22:08:59.202885
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '77e77aae3811'
+revision = '71415cf8670f'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,6 +21,7 @@ def upgrade():
     op.create_table('users',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=100), nullable=False),
+    sa.Column('password', sa.String(length=256), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
